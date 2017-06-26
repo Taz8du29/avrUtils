682f2589e60a6db4cd12b435e93bc132bff9f11a
Taz8du29 <coding@taz8du29.fr>
2017-06-27 00:30:00 +0200

    Updated glcd_ssd1306 header

--- displays/glcd_ssd1306.h
+++ displays/glcd_ssd1306.h
@@ -1,6 +1,7 @@
 /* glcd_ssd1306.h
  *
- * Drivers for the SSD1306 driven LCD displays, over I²C
+ * SSD1306 based graphical LCD driver
+ * With I²C interface
  *
  * Copyright 2015 - 2017 (C) Taz8du29
  * Refer to LICENSE.MD for more infos about copyright
@@ -10,11 +11,13 @@
 #ifndef _GLCD_SSD1306_H_
 #define _GLCD_SSD1306_H_
 
+
 /* DEPENDENCIES */
 
 #if !defined( _I2C_MASTER_H_ ) || !defined( _MYUTILS_H_ )
 	#error This file needs the following dependencies : myUtils, i2c_master
 #endif
+
 
 
 /* SSD1306 CONSTANTS */
@@ -23,13 +26,10 @@
 #define glcd_inverseDisplay_CMD 0xA7
 
 #define glcd_displayOff 0xAE
-#define glcd_displayOn 0xAF
+#define glcd_displayOn  0xAF
 
-#define glcd_
-#define glcd_
-
-#define glcd_
-#define glcd_
+//#define glcd_
+//#define glcd_
 
 
 
@@ -42,18 +42,7 @@ static inline void glcd_sendCommand(uint8_t command) __attribute__((always_inlin
 
 
 /* SSD1306 FUNCTIONS */
-
-void glcd_init() {
-	// Init I²C connection
-	i2c_init();
-
-	//
-}
-
-void glcd_sendCommand(uint8_t command) {
-	i2c_send()
-}
+#include "glcd_ssd1306.c"
 
 
-
-#endif
+#endif  /* !_GLCD_SSD1306_H_ */
